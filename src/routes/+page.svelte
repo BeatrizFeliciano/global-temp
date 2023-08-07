86e485b0f991133e1abdc0361cf574640b67e20a
Beatriz Feliciano <beatrizfeliciano1999@hotmail.com>
2023-08-07 21:00:43 +0000

    added legend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 	import Tooltip from './Tooltip.svelte';
 	import { scaleLinear } from 'd3';
 
-	let innerWidth = 0
-    let innerHeight = 0
+	export let innerWidth;
+    export let innerHeight;
 
 	let warmColor = scaleLinear()
         .domain([0, 1.3])
@@ -13,6 +13,16 @@
 	let coldColor = scaleLinear()
 		.domain([-1.3, 0])
 		.range(["#0805b1", "#e6e6e6"]);
+
+	let colorRange = (value) => {
+		if (value > 0)
+			return warmColor(value)
+		else if (value < 0)
+			return coldColor(value)
+		else if (value === 0)
+			return "#e6e6e6"
+		return "transparent"
+	}
 
 	let month = (index) => {
 		let months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
@@ -34,17 +44,6 @@
 
 		return "nothing"
 	}
-
-	let calcColor = (fig, value) => {
-		if (fig === 'circle')
-			return "background-color: " + warmColor(value)
-
-		else if (fig === 'triangle')
-			return "background-color: " + coldColor(value)
-
-		return ""
-	}
-	
 </script>
 
 <svelte:head>
@@ -59,7 +58,7 @@
 	<h2 class="text">
 		<p>
 			The average global temperature on Earth has 
-			<u style={"text-decoration-thickness: 3px; text-decoration-color: " + warmColor(1.1)}>
+			<u style="text-decoration-thickness: 3px; text-decoration-color: {warmColor(1.1)}">
 				increased around 1.1° Celsius
 			</u> 
 			since 1880.
@@ -69,26 +68,40 @@
 			affect the temperature of the all the Earth's sea surface and air near-surface.
 		</p>
 	</h2>
+
 	<div class="viz">
+		<div class="legend">
+			<div class="legend-elements">
+				<div class="triangle" style="width: 20px; height: 20px; transform: translate(-20px);"/>
+				<div class="square" style="width: 20px; height: 20px;"/>
+				<div class="circle" style="width: 20px; height: 20px; transform: translate(20px);"/>
+			</div>
+			<div class="color-scale"/>
+			<div class="legend-elements" style="font-size: x-small;">
+				<p style="transform: translate(-10px);">-1.3ºC</p>
+				<p>0ºC</p>
+				<p style="transform: translate(10px);">1.3ºC</p>
+			</div>
+		</div>	
 		{#each data as d}
 			<div class="data">
 				<p class="year">{d.year}</p>
 				{#each Array(12) as _, index}
 					{#if figure(parseFloat(d[month(index)])) !== "nothing"}
 						<Tooltip 
-							title="value: {parseFloat(d[month(index)])}"
+							title="value: {parseFloat(d[month(index)])}ºC"
 						>
 							<div 
 								class={figure(parseFloat(d[month(index)]))} 
 								key={index}
-								style={calcColor(figure(parseFloat(d[month(index)])), parseFloat(d[month(index)]))}
+								style="background-color: {colorRange(parseFloat(d[month(index)]))}"
 							/>
 						</Tooltip>
 					{:else}
 						<div 
 							class={figure(parseFloat(d[month(index)]))} 
 							key={index}
-							style={calcColor(figure(parseFloat(d[month(index)])), parseFloat(d[month(index)]))}
+							style="background-color: {colorRange(parseFloat(d[month(index)]))}"
 						/>
 					{/if}
 				{/each}
@@ -106,21 +119,11 @@
 		flex: 1;
 	}
 
-	.year {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		margin-right: 1.5vw;
-		font-size: clamp(10px, 2vw, 16px);
-		text-align: justify;
-	}
-
 	.title {
-		color: rgb(241, 241, 241);
+		color: rgb(193, 193, 193);
 		margin-bottom: 30px;
 		text-align: center;
-		font-size: clamp(30px, 50px);
+		font-size: clamp(30px, 500px);
 	}
 
 	.text {
@@ -145,10 +148,42 @@
 		align-items: center;
 	}
 
+	.year {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		margin-right: 1.5vw;
+		font-size: clamp(10px, 2vw, 16px);
+		text-align: justify;
+	}
+
+	.legend {
+		width: auto;
+		height: auto;
+		margin-bottom: 30px;
+	}
+
+	.legend-elements {
+		display: flex;
+		width: 50vw;
+		height: auto;
+		justify-content: space-between;
+		align-items: flex-start;
+	}
+
+	.color-scale {
+		width: 50vw;
+		height: 10px;
+		background: linear-gradient(90deg, #0805b1 0%, #e6e6e6 50%, #810101 100%);
+		border-radius: 10px;
+	}
+
 	.circle {
 		border-radius: 50%;
 		width: 5vw;
 		height: 5vw;
+		background-color: #810101;
 		max-width: 50px;
 		max-height: 50px;
 		min-width: 15px;
@@ -159,6 +194,7 @@
 	.triangle {
 		width: 5vw;
 		height: 5vw;
+		background-color: #0805b1;
 		max-width: 50px;
 		max-height: 50px;
 		min-width: 15px;
@@ -181,7 +217,6 @@
 	}
 
 	.nothing {
-		background-color: transparent;
 		margin: 1vw;
 		width: 5vw;
 		height: 5vw;
