36b170e3a4311b758e628dab26522456b57227c2
Beatriz Feliciano <beatrizfeliciano1999@hotmail.com>
2023-08-13 15:07:50 +0100

    changed legend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,8 @@
 	export let innerWidth;
     export let innerHeight;
 
+	let colorStep = 0.1857;
+
 	let warmColor = scaleLinear()
         .domain([0, 1.3])
         .range(["#e6e6e6", "#810101"]);
@@ -70,11 +72,18 @@
 	</h2>
 
 	<div class="viz">
+
 		<div class="legend">
-			<div class="legend-elements">
-				<div class="triangle" style="width: 20px; height: 20px; transform: translate(-20px);"/>
-				<div class="square" style="width: 20px; height: 20px;"/>
-				<div class="circle" style="width: 20px; height: 20px; transform: translate(20px);"/>
+			<div class="legend-elements" style="width: 52.5vw;">
+				{#each Array(7) as _, i}
+					<div class="triangle-legend" style="background-color: {coldColor(-1.3 + colorStep*i)};"/>
+				{/each}
+
+				<div class="square-legend"/>
+
+				{#each Array(7) as _, i}
+					<div class="circle-legend" style="background-color: {warmColor(0 + colorStep*i)};"/>
+				{/each}
 			</div>
 			<div class="color-scale"/>
 			<div class="legend-elements" style="font-size: x-small;">
@@ -196,16 +205,46 @@
 	.legend {
 		width: auto;
 		height: auto;
-		margin-bottom: 40px;
+		margin-bottom: 30px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 
 	.legend-elements {
 		display: flex;
+		flex-direction: row;
+		align-items: center;
 		width: 50vw;
 		height: auto;
 		justify-content: space-between;
 		align-items: flex-start;
 		margin-bottom: 5px;
+	}
+
+	.circle-legend {
+		border-radius: 50%;
+		width: 2vw;
+		height: 2vw;
+		background-color: #810101;
+		margin: 0.5vw;
+	}
+
+	.triangle-legend {
+		width: 2vw;
+		height: 2vw;
+		background-color: #0a07e2;
+		clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+		margin: 0.5vw;
+		box-sizing: border-box;
+	}
+
+	.square-legend {
+		background-color: #e3e3e3;
+		margin: 0.5vw;
+		border-radius: 5px;
+		width: 2vw;
+		height: 2vw;
 	}
 
 	.color-scale {
